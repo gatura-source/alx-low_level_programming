b9059af34d0a48079d78c620e760e7ae34f67e49
gatura-source <bedanjedidiah@gmail.com>
2023-03-16 14:23:49 -0700

    task 6 amend betty correct output

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,11 +8,11 @@ int main(void)
 {
 
 	int a; long int b; long long int c; char d; float f;
-	printf("Size of a char is: %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("Size of an int is: %lu byte(s)\n", (unsigned long)sizeof(a));
-	printf("Size of an long int is: %lu byte(s)\n", (unsigned long)sizeof(b));
-	printf("Size of a long long int is: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of a floating point variable is: %lu byte(s)\n", (unsigned long)sizeof(f));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of an long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 
 	return (0);
 }
