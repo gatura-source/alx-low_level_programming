3dec32e465e107abcb64ca84f892800c009603f0
gatura-source <bedanjedidiah@gmail.com>
2023-03-16 15:05:37 -0700

    betty and 5 output

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-int main (void)
+/**
+ * main - prints out a statement
+ * Description: as said above
+ * Return: 0 it is a puts program
+ */
+int main(void)
 {
 	puts("Programming is like building a multilingual puzzle\n");
 
