f3041784012b5a8d0fc41b196a528ed3ba5a8dc7
gatura-source <bedanjedidiah@gmail.com>
2023-03-16 14:21:39 -0700

    task 6 amend betty coding style

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,15 +4,15 @@
  * Program prints size of various data types used in C
  *
  */
-int main (void)
+int main(void)
 {
 
 	int a; long int b; long long int c; char d; float f;
-	printf("Size of a char is :: %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("Size of an int is :: %lu byte(s)\n", (unsigned long)sizeof(a));
-	printf("Size of an long int is :: %lu byte(s)\n", (unsigned long)sizeof(b));
-	printf("Size of a long long int is:: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of a floating point variable is:: %lu byte(s)\n", (unsigned long)sizeof(f));
+	printf("Size of a char is: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of an int is: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of an long int is: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int is: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a floating point variable is: %lu byte(s)\n", (unsigned long)sizeof(f));
 
 	return (0);
 }
