692fa80197fe8476a5d5bf3c77a63f3a672da2e1
gatura-source <bedanjedidiah@gmail.com>
2023-03-16 15:02:12 -0700

    betty for 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
-
-int main (void)
+/**
+ * main - prints a statement
+ * Description: as said above
+ * Return: its a print statement so always 0
+ */
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art\n");
 
-	return 0;
+	return (0);
 }
