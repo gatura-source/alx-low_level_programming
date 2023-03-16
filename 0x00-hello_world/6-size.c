f81bed7c58c9c284e24473e1be72c8c3c4761bf4
gatura-source <bedanjedidiah@gmail.com>
2023-03-16 14:30:00 -0700

    formatting to betty style

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 /*
  *
+ * main-Entry Point
  * Program prints size of various data types used in C
  *
  */
 int main(void)
 {
 
-	int a; long int b; long long int c; char d; float f;
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
