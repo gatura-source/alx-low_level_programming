05ec2fa5cd3f2719f89223a253ed32a35ccfd1b1
gatura-source <bedanjedidiah@gmail.com>
2023-03-17 14:56:22 -0700

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -25,7 +25,7 @@ int main(void)
 	}
 	else if (m < 6 && m != 0)
 	{
-		printf("Last digit of %d is %d and is less than 6\n", n, m);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
 	}
 	else if (m == 0)
 	{
