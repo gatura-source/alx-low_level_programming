bf47421bdbde47f4b0babc2cad12d3ccb5c964b2
gatura-source <bedanjedidiah@gmail.com>
2023-03-17 15:53:29 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: prints 0-9
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d", a);
+	}
+	putchar(10);
+	return  (0);
+}
