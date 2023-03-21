dfaa443ae80c3410ca36e163b07a2d2058f5fc7d
gatura-source <bedanjedidiah@gmail.com>
2023-03-21 13:54:20 -0700

    fixing header file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,5 @@
 #include "main.h"
 /**
- * print_alphabet - prints alphabet in lowercase
- * Description: prints alphabet in lowercase
- * Return: void
- */
-void print_alphabet(void)
-{
-	int b;
-
-	for (b = 97; b < 123; b++)
-	{
-		_putchar(b);
-	}
-}
-/**
  * main - entry point
  * Description: prints alphabet to stdout
  * Return: always zero
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -9,3 +9,18 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
+/**
+ * print_alphabet - prints alphabet to stdout
+ * Description: as seen above
+ * Return: 1 always
+ */
+void print_alphabet(void)
+{
+	int a;
+
+	for (a = 97; a < 123; a++)
+	{
+		_putchar(a);
+	}
+}
