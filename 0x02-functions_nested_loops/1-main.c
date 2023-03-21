225b6086d65420783ca8dd0dd788aa1453a6774d
gatura-source <bedanjedidiah@gmail.com>
2023-03-21 14:02:57 -0700

    task 1 finla fix on header files

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - as called
+ * Description: as called
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	int b;
+
+	for (b = 97; b < 123; b++)
+	{
+		_putchar(b);
+	}
+}
