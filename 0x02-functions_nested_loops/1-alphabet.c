18f87a61e429b7928badae23e9924ec5550e414c
gatura-source <bedanjedidiah@gmail.com>
2023-03-21 12:02:15 -0700

    task 1 betty

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,19 @@
 #include "main.h"
 /**
  * main - entry point
- * Description - prints alphabet in lowercase
- * Return - always 0
+ * Description: prints alphabet in lowercase
+ * Return: always 0
  */
 int main(void)
 {
 	print_alphabet();
 	return (0);
 }
-
+/**
+ * print_alphabet - prints alphabet in lowercase
+ * Description: prints alphabet in lowercase
+ * Return: void
+ */
 void print_alphabet(void)
 {
 	int a;
