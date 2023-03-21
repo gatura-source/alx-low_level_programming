a1b72b6e6451df5b337d5b0ca6dbfd368b1c32d9
gatura-source <bedanjedidiah@gmail.com>
2023-03-21 16:41:58 -0700

    (task 6)

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - RTFC
+ * Description: RTFC
+ * Return: 0
+ */
+int _abs(int num)
+{
+	int r;
+
+	if (num >= 0)
+	{
+		r = num;
+	}
+	else
+	{
+		r = ((num) * (-1));
+	}
+	return (r);
+}
