e5c734fdb2fb12051d45378f231292d4f931477c
gatura-source <bedanjedidiah@gmail.com>
2023-03-23 04:06:42 -0700

    (task 2)

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiplies two ints
+ * @a: func input 1
+ * @b: func input 2
+ *
+ * Return: a*b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,5 +3,6 @@
 
 int _isupper(int c);
 int _isdigit(int d);
+int mul(int a, int b);
 
 #endif
