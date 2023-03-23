7055c65aa77f71e4e46871516cd1a325edab702f
gatura-source <bedanjedidiah@gmail.com>
2023-03-23 08:17:42 -0700

    (task 7)

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,24 +12,24 @@ void print_diagonal(int n)
 	int j;
 	int i;
 
-	 j = 0;
+	j = 0;
 
-	 while (n > 0)
-	 {
-		 i = j;
-		 while (i > 0)
-		 {
-			 _putchar(32);
-			 i--;
-		 }
-		 _putchar(92);
-		 _putchar('\n');
-		 j++;
-		 n--;
-	 }
-	 if (j < 1)
-	 {
-		 _putchar('\n');
-	 }
+	while (n > 0)
+	{
+		i = j;
+		while (i > 0)
+		{
+			_putchar(32);
+			i--;
+		}
+		_putchar(92);
+		_putchar('\n');
+		j++;
+		n--;
+	}
+	if (j < 1)
+	{
+		_putchar('\n');
+	}
 }
 
