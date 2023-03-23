eea1263c914d215d8527f57e81439868bc682bba
gatura-source <bedanjedidiah@gmail.com>
2023-03-23 05:57:19 -0700

    (task 7 betty and loop values)

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,13 +11,13 @@ void print_line(int n)
 {
 	int i;
 
-	if ( n <= 0)
+	if (n <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		for (i = 0; i <= n; i++)
+		for (i = 1; i <= n; i++)
 		{
 			_putchar(95);
 		}
