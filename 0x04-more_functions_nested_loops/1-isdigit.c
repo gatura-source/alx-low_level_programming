5539c414bc9165b72bdadec9e2f89d0a17b952ff
gatura-source <bedanjedidiah@gmail.com>
2023-03-23 03:56:20 -0700

    (task 1)

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isdigit - checks if digit in 0-9
+ * @d: func input
+ *
+ * Return: 1 is digit, 0 otherwise
+ */
+
+int _isdigit(int d)
+{
+	int r;
+
+	if (d >= 48 && d >= 57)
+	{
+		r = 1;
+	}
+	else
+	{
+		r = 0;
+	}
+
+	return (r);
+}
