bc98935822f50bfdd21e2d5bb2f7664e31bbc968
gatura-source <bedanjedidiah@gmail.com>
2023-03-23 05:54:47 -0700

    (task 6)

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,9 +11,16 @@ void print_line(int n)
 {
 	int i;
 
-	for (i = 0; i <= n; i++)
+	if ( n <= 0)
 	{
-		_putchar(45);
+		_putchar('\n');
 	}
-	_putchar('\n');
+	else
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
 }
