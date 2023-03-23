c2dccf8fff39bd44cfbb1ab1a25f06683cdf37f0
gatura-source <bedanjedidiah@gmail.com>
2023-03-23 06:56:57 -0700

    (task 7)

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * helper_f - helper function for print_diagonal
+ * @m: func input
+ *
+ * Return: void
+ */
+void helper_f(int m)
+{
+	int i;
+
+	if (m <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= m; i++)
+		{
+			_putchar(32);
+		}
+	}
+}
+/**
+ * print_diagonal - prints values diagonal
+ * @n: func input
+ *
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (j = 1; j <= n; j++)
+		{
+			helper_f(j);
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
+
