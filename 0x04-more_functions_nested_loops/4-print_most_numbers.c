10d4fabdff87c28e0b1fcb7317ece181b48ca915
gatura-source <bedanjedidiah@gmail.com>
2023-03-23 04:42:37 -0700

    (task 4)

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints 0-9 excluding 2 and 4
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		if (n != 50 && n != 522)
+		{
+			_putchar(n);
+		}
+	}
+	_putchar('\n');
+}
+
