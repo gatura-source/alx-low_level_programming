d32dc466eb17a8ed955c09143d9480265aac20de
gatura-source <bedanjedidiah@gmail.com>
2023-03-27 08:26:48 -0700

    (task 3 pointers)

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear";
+	_puts(str);
+	return (0);
+}
+
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - outputs string to stdout
+ * @str: string inut
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
