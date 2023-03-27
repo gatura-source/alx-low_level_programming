3aa4ae2c806e1fc6e12adff07fb5fce66911eb1c
gatura-source <bedanjedidiah@gmail.com>
2023-03-27 09:20:38 -0700

    (task 4 edit two on newline character)

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 
 /**
  * print_rev - prints str in reverse
@@ -11,6 +12,7 @@
 void print_rev(char *s)
 {
 	int len;
+	char str[strlen(s)]; /** declaring a string same size as input */
 
 	len = 0;
 
@@ -23,4 +25,5 @@ void print_rev(char *s)
 		putchar(s[len]);
 		len--;
 	}
+	puts(str);
 }
