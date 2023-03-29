2066504082e2c32006d35d4c27e040e2cc62c8ae
gatura-source <bedanjedidiah@gmail.com>
2023-03-29 04:53:47 -0700

    (task 2)

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncpy - works exacty like strcpy
+ * @dest: dest input
+ * @src: src input
+ * n: chars to be copied
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+
+	return (dest);
+}
