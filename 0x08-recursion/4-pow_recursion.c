9ab2b2daddbae2e8339103c9eaa6b5b52a63c392
gatura-source <bedanjedidiah@gmail.com>
2023-04-05 13:25:38 -0700

    (task 4)

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - pow but with recursion
+ * @x: value to be exponentiated
+ * @y: exponent
+ *
+ * Return: int
+ */
+
+int _pow_recursion(int x, int y)
+{
+
+	if (y > 0)
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+	else
+	{
+		return (1);
+	}
+
+}
