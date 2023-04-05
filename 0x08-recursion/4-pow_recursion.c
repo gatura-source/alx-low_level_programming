99e689c45f9708c71f351651fbbf5eb45c335041
gatura-source <bedanjedidiah@gmail.com>
2023-04-05 13:37:34 -0700

    (task 4)

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -15,14 +15,13 @@ int _pow_recursion(int x, int y)
 	{
 		return (x * _pow_recursion(x, y - 1));
 	}
+	else if (y < 0)
+	{
+		return (-1);
+	}
 	else
 	{
 		return (1);
-	}
-
-	if (y < 0)
-	{
-		return (-1);
 	}
 
 }
