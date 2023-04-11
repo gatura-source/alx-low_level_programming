e9cd14fcd9ccb408d461cf9c250086bbf51e6357
gatura-source <bedanjedidiah@gmail.com>
2023-04-11 04:44:49 -0700

    (task 0 edit one on pointer position shift)

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,17 +12,21 @@
 char *create_array(unsigned int size, char c)
 {
 	char *ptr;
+	unsigned int i;
 
 	ptr = malloc(size);
 
-	if (ptr == NULL)
+	if (ptr == NULL || size <= 0)
 	{
 		return (NULL);
 	}
 
 	else
 	{
-		ptr[0] = c;
+		for (i = 0; i < size; i++)
+		{
+			ptr[i] = c;
+		}
 	}
 
 	return (ptr);
