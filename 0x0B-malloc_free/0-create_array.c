b2802cf021d5894a3dd471c302cc62761cf098e5
gatura-source <bedanjedidiah@gmail.com>
2023-04-11 04:22:14 -0700

    (task 0 on malloc)

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - array with user-defined values
+ * @size: array size
+ * @c: char initializer
+ *
+ * Return: pointer to array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+
+	ptr = malloc(size);
+
+	if (*ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	else
+	{
+		ptr[0] = c;
+	}
+
+	return (ptr);
+
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,3 @@
+Dir contains tasks on malloc and free
+Task 0 is writing a program that allows one to create an array of chars using a user-defined memory value and a char as an initializer
+prototype: char *create_array(unsigned int size, char c)
