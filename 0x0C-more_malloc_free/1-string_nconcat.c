70485804d00cc24d8820c0e9af9351a9963ae47a
gatura-source <bedanjedidiah@gmail.com>
2023-04-13 15:35:22 -0700

    (task 1 is a bummer)

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,34 +1,45 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 /**
- * concat - does the real concatenation
- * @ptr: pointer to mem of concatenation
- * @ptrarray: array cont. strings
+ * concat - does the heavy lifting
+ * @ptr: pointer
+ * @ptrarray: array containing s1 and s2
  * @n: bytes to copy
- *
  * Return: pointer
  */
 char *concat(char *ptr, char *ptrarray[], size_t n)
 {
-	unsigned int i;
-	unsigned int j;
+	size_t i;
+	size_t j;
 
 	for (i = 0; i < 2; i++)
 	{
-		if (i == 0)
+		if (ptrarray[i] == NULL)
+		{
+			;
+		}
+		else if ((i == 0) && (ptrarray[i] != NULL))
 		{
 			for (j = 0; j < strlen(ptrarray[i]); j++)
 			{
 				ptr[j] = (ptrarray[i])[j];
 			}
 		}
-		if (i == 1)
+		else if ((i == 1) && (ptrarray[i] != NULL) && (ptrarray[i - 1] != NULL))
 		{
 			for (j = 0; j < n; j++)
 			{
-				ptr[strlen(ptrarray[i - 1]) + j] = (ptrarray[i])[j];
+				ptr[j + strlen(ptrarray[i - 1])] = (ptrarray[i])[j];
+			}
+		}
+		else
+		{
+			for (j = 0; j < strlen(ptrarray[i]); j++)
+			{
+				ptr[j] = (ptrarray[i])[j];
 			}
 		}
 	}
@@ -44,41 +55,34 @@ char *concat(char *ptr, char *ptrarray[], size_t n)
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	size_t p;
 	char *ptr;
-	size_t len_1;
-	size_t len_2;
-	char *ptr_2;
+	char *pt_r;
+	size_t i;
 	char *ptrarray[2];
 
-	len_1 = 0;
-	len_2 = 0;
-	if (s1 == NULL)
+	p = 0;
+	ptrarray[0] = s1;
+	ptrarray[1] = s2;
+	for (i = 0; i <  2; i++)
 	{
-		s1 = "";
+		if (ptrarray[i] != NULL && i == 0)
+		{
+			p += strlen(s1);
+		}
+		else if (ptrarray[i] != NULL && i == 1)
+		{
+			p += n;
+		}
 	}
-	if (s2 == NULL)
-	{
-		s2 = "";
-	}
-	while (s1[len_1] != '\0')
-	{
-		len_1++;
-	}
-	while (s2[len_2] != '\0')
-	{
-		len_2++;
-	}
-	if (n >= len_2)
-	{
-		n = len_2;
-	}
-	ptr = malloc(n + 1 + len_1);
+	ptr = malloc(p + 1);
 	if (ptr == NULL)
 	{
 		return (NULL);
 	}
-	ptrarray[0] = s1;
-	ptrarray[1] = s2;
-	ptr_2 = concat(ptr, ptrarray, n);
-	return (ptr_2);
+	else
+	{
+		pt_r = concat(ptr, ptrarray, n);
+	}
+	return (pt_r);
 }
