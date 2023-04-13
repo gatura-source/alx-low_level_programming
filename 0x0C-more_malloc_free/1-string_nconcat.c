b898bae9201faab42f666e6c9c23275b89f6dfae
gatura-source <bedanjedidiah@gmail.com>
2023-04-13 15:30:23 -0700

    (Code reversion)

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,39 @@
 #include "main.h"
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+/**
+ * concat - does the real concatenation
+ * @ptr: pointer to mem of concatenation
+ * @ptrarray: array cont. strings
+ * @n: bytes to copy
+ *
+ * Return: pointer
+ */
+char *concat(char *ptr, char *ptrarray[], size_t n)
+{
+	unsigned int i;
+	unsigned int j;
+
+	for (i = 0; i < 2; i++)
+	{
+		if (i == 0)
+		{
+			for (j = 0; j < strlen(ptrarray[i]); j++)
+			{
+				ptr[j] = (ptrarray[i])[j];
+			}
+		}
+		if (i == 1)
+		{
+			for (j = 0; j < n; j++)
+			{
+				ptr[strlen(ptrarray[i - 1]) + j] = (ptrarray[i])[j];
+			}
+		}
+	}
+	return (ptr);
+}
 /**
  * string_nconcat - adds twos strings up
  * @s1: string one
@@ -14,8 +47,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *ptr;
 	size_t len_1;
 	size_t len_2;
-	unsigned int i;
+	char *ptr_2;
+	char *ptrarray[2];
 
+	len_1 = 0;
+	len_2 = 0;
 	if (s1 == NULL)
 	{
 		s1 = "";
@@ -24,8 +60,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s2 = "";
 	}
-	len_1 = strlen(s1);
-	len_2 = strlen(s2);
+	while (s1[len_1] != '\0')
+	{
+		len_1++;
+	}
+	while (s2[len_2] != '\0')
+	{
+		len_2++;
+	}
 	if (n >= len_2)
 	{
 		n = len_2;
@@ -35,13 +77,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < len_1; i++)
-	{
-		ptr[i] = s1[i];
-	}
-	for (i = 0; i < n; i++)
-	{
-		ptr[len_1 + i] = s2[i];
-	}
-	return (ptr);
+	ptrarray[0] = s1;
+	ptrarray[1] = s2;
+	ptr_2 = concat(ptr, ptrarray, n);
+	return (ptr_2);
 }
