bca21382d57956b7134e4e05067fa6689a073ec6
gatura-source <bedanjedidiah@gmail.com>
2023-04-19 04:27:05 -0700

    (task 2)

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,45 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+#include <stdbool.h>
+/**
+ * int_index - searches for int int an array
+ * @array: array input
+ * @size: array size
+ * @cmp: function used to compare values
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+
+	int iter;
+	bool ret;
+
+	ret = false;
+	iter = 0;
+	if (array != NULL && cmp != NULL)
+	{
+		if (size >= 0)
+		{
+			while (iter < size)
+			{
+				ret = cmp(array[iter]);
+				if (ret == true)
+				{
+					return (iter);
+				}
+				iter++;
+			}
+		}
+		else
+		{
+			return (-1);
+		}
+	}
+	else
+	{
+		return (-1);
+	}
+
+	return (0);
+}
