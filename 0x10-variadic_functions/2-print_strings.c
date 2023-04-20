391771997fa7ffad373b9b1f6b46c637a47cab50
gatura-source <bedanjedidiah@gmail.com>
2023-04-20 14:58:48 -0700

    (task 2 edit on (nul))

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -24,7 +24,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			string = va_arg(args_ptr, char *);
 			if (string == NULL)
 			{
-				printf("nil");
+				printf("(nil)");
 			}
 			else
 			{
