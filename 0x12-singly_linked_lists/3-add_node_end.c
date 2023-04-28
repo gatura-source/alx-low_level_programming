663675a5f28b52e0b4646a599c7b4bf624d9529a
gatura-source <bedanjedidiah@gmail.com>
2023-04-27 17:55:08 -0700

    (task 3 edit on head traversal)

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -12,12 +12,21 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *add;
+	list_t *temp;
 
 	if (*head == NULL)
 	{
-		(*head)->str = strdup(str);
-		(*head)->len = strlen(str);
-		(*head)->next = NULL;
+		*head = malloc(sizeof(list_t));
+		if (*head != NULL)
+		{
+			(*head)->str = strdup(str);
+			(*head)->len = strlen(str);
+			(*head)->next = NULL;
+		}
+		else
+		{
+			return (NULL);
+		}
 	}
 	else
 	{
@@ -27,7 +36,12 @@ list_t *add_node_end(list_t **head, const char *str)
 			add->str = strdup(str);
 			add->len = strlen(str);
 			add->next = NULL;
-			(*head)->next = add;
+			temp = *head;
+			while (temp->next != NULL)
+			{
+				temp = temp->next;
+			}
+			temp->next = add;
 		}
 		else
 		{
