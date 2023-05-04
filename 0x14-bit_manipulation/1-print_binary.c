417ae3cedf9857060add0fe5a2e44cab3e8725e4
gatura-source <bedanjedidiah@gmail.com>
2023-05-04 15:39:51 -0700

    (task 1 edit 2)

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,5 +9,30 @@
 
 void print_binary(unsigned long int n)
 {
-	
+	int ULBITS_SIZE;
+	unsigned long int MASK;
+	int one_flag;
+
+	one_flag = 0;
+	ULBITS_SIZE = sizeof(unsigned long int) * 8 - 1;
+	MASK = 1UL;
+	MASK <<= ULBITS_SIZE;
+
+	while (MASK > 0)
+	{
+		if (n & MASK)
+		{
+			_putchar('1');
+			one_flag = 1;
+		}
+		else if (one_flag)
+		{
+			_putchar('0');
+		}
+		MASK >>= 1;
+	}
+	if (!one_flag)
+	{
+		_putchar('0');
+	}
 }
