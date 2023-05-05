9e62c8d9febe2e5302f4470c1d244a3e369be1a9
gatura-source <bedanjedidiah@gmail.com>
2023-05-05 04:26:01 -0700

    (task 3)

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,5 +5,6 @@ double power(double base, int expo);
 void print_binary(unsigned long int n);
 int _putchar(char i);
 int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
 
 #endif
