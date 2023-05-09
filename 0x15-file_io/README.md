c309cc5f63bb4b80250b80cc6e363f5ef9b841ff
gatura-source <bedanjedidiah@gmail.com>
2023-05-08 17:51:49 -0700

    (task 0 on FILEIO)

---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+Repo contains tasks on FILE IO
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,7 @@
+#ifndef FILEIO_H
+#define FILEIO_H
+
+ssize_t read_textfile(const char *filename, size_t letters);
+void *e_malloc(unsigned int mem_size);
+
+#endif
