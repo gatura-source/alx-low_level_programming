584723cb905194b4962b7dca0790d90fc74cbf37
gatura-source <bedanjedidiah@gmail.com>
2023-05-09 04:24:59 -0700

    (task 2 edit 1000)

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -41,7 +41,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_CREAT | O_WRONLY | O_EXECL, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_CREAT | O_WRONLY | O_EXCL, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 	{
 		fd = open(filename, O_TRUNC);
