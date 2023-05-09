7a22fea479d7ea674cb83cb04591f51638aba208
gatura-source <bedanjedidiah@gmail.com>
2023-05-08 19:21:48 -0700

    (task 2)

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,11 @@
 #ifndef FILEIO_H
 #define FILEIO_H
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stddef.h>
+#include <string.h>
 /**
  * err_malloc - Error-checked malloc
  * @memsize: Dynamic mem requested
@@ -10,5 +15,6 @@
 void *e_malloc(unsigned int memsize);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
 #endif
