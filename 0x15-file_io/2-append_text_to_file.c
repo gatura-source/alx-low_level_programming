243fa2bd13d28a446ffc4def2933f45471c46957
gatura-source <bedanjedidiah@gmail.com>
2023-05-09 04:51:58 -0700

    (task 2 edit 2)

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,13 +21,12 @@ int append_text_to_file(const char *filename, char *text)
 	{
 		return (-1);
 	}
-	if (text == NULL)
+	if (text != NULL)
 	{
-		;
-	}
-	if (write(fd, text, strlen(text)) == -1)
-	{
-		return (-1);
+		if (write(fd, text, strlen(text)) == -1)
+		{
+			return (-1);
+		}
 	}
 	close(fd);
 	return (1);
