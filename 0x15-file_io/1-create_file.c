b40c79301fe71c9a9c2283f056737dd9cf9193b0
gatura-source <bedanjedidiah@gmail.com>
2023-05-09 04:46:27 -0700

    (task 1)

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -52,7 +52,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (text_content != NULL)
 	{
-		in_buffer= err_malloc(sizeof(char) * strlen(text_content));
+		in_buffer = err_malloc(sizeof(char) * strlen(text_content));
 		strcpy(in_buffer, text_content);
 		if (write(fd, in_buffer, strlen(in_buffer)) == -1)
 		{
