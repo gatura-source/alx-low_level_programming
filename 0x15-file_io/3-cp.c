1a0ec7fc9cfecb77d424f88f9088c3b009c0f60c
gatura-source <bedanjedidiah@gmail.com>
2023-05-09 07:34:13 -0700

    (task cp)

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,7 +9,6 @@
 /**
  * err_malloc - error checked malloc
  * @mem: Dynamic mem needed
- *
  * Return: ptr or NULL
  */
 void *err_malloc(unsigned int mem)
@@ -25,10 +24,40 @@ void *err_malloc(unsigned int mem)
 
 }
 /**
+ * usage - Usage
+ * @prog: program
+ *
+ * Return: None
+ */
+void usage(char *prog)
+{
+	dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", prog);
+	exit(97);
+}
+/**
+ * close_fd - close fds
+ * @fd1: fd 1
+ * @fd2: fd 2
+ *
+ * Return: void
+ */
+void close_fd(int fd1, int fd2)
+{
+	if (close(fd1) != 0)
+	{
+		dprintf(STDERR_FILENO, "can't close fd %i\n", fd1);
+		exit(100);
+	}
+	if (close(fd2) != 0)
+	{
+		dprintf(STDERR_FILENO, "Can't close fd %i\n", fd2);
+		exit(100);
+	}
+}
+/**
  * main - like normal cp
  * @argc: counter
  * @argv: vector
- *
  * Return: 0 on success
  */
 int main(int argc, char *argv[])
@@ -40,49 +69,38 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
-		exit(97);
+		usage(argv[0]);
 	}
 	if (argc == 3)
 	{
-		fdin = open(argv[2], O_WRONLY | O_EXCL | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-		if (fdin == -1)
-		{
-			fdin = open(argv[2], O_WRONLY | O_TRUNC);
-			if (fdin == -1)
-			{
-				dprintf(STDERR_FILENO, "Can\'t write to %s\n", argv[2]);
-				exit(99);
-			}
-		}
-		if ( (fdout = open(argv[1], O_RDONLY)) != -1)
-		{
-			buffout = err_malloc(sizeof(char) * 1024);
-			while ((byte_count = read(fdout, buffout, sizeof(buffout))) > 0)
-			{
-				if (write(fdin, buffout, byte_count) == (ssize_t) -1)
-				{
-					dprintf(STDERR_FILENO, "Can\'t write to %s\n", argv[2]);
-					exit(99);
-				}
-				bzero(buffout, sizeof(char) * 1024);
-			}
-		}
-		else
+		fdout = open(argv[1], O_RDONLY);
+		if (fdout == -1)
 		{
 			dprintf(STDERR_FILENO, "Can't read from %s\n", argv[1]);
 			exit(98);
 		}
+		fdin = open(argv[2], O_WRONLY | O_CREAT | O_EXCL, 0664);
+		if (fdin == -1)
+		{
+			fdin = open(argv[2], O_TRUNC | O_WRONLY);
+			if (fdin == -1)
+			{
+				dprintf(STDERR_FILENO, "Can't write to %s\n", argv[2]);
+				exit(99);
+			}
+		}
+		buffout = err_malloc(sizeof(char) * 1024);
+		while ((byte_count = read(fdout, buffout, sizeof(buffout))) > 0)
+		{
+			if (write(fdin, buffout, byte_count) == (ssize_t) -1)
+			{
+				dprintf(STDERR_FILENO, "Can\'t write to %s\n", argv[2]);
+				exit(99);
+			}
+			bzero(buffout, sizeof(char) * 1024);
+		}
 		free(buffout);
-		if (close(fdin) != 0)
-		{
-			dprintf(STDERR_FILENO, "Can't close fd %i\n", fdin);
-			exit(100);
-		}
-		if (close(fdout) != 0)
-		{
-			dprintf(STDERR_FILENO, "Can't close fd %i\n", fdout);
-		}
+		close_fd(fdin, fdout);
 	}
 	return (0);
-}		
+}
