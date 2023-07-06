a62365bb506276686a57dd0a1a6b893f069d486a
gatura-source <bedanjedidiah@gmail.com>
2023-07-06 09:10:57 -0700

    3-set_bit correction 1

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * set_bit - sets a bit at a given position
  * @n: Value
@@ -14,9 +14,9 @@ int set_bit(unsigned long int *n, unsigned int index)
 	unsigned int size;
 	unsigned long int _n;
 
-	size = sizeof(n) * 16 - 1;
+	size = sizeof(n) * 8 - 1;
 	_n = *n;
-	if (index < size)
+	if (index <= size)
 	{
 
 		mask = 1 << index;
