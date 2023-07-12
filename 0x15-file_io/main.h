73df108ee147b3b26e6bab8028b2f74a772f7727
gatura-source <bedanjedidiah@gmail.com>
2023-07-12 11:23:04 -0700

    1-create_file 1

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+#include <sys/types.h>
+#include <stddef.h>
 ssize_t read_textfile(const char *, size_t);
 void *err_malloc(size_t);
 int create_file(const char *, char *);
